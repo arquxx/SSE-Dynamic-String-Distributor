07b3f60de9b3f4b0d54ae6786ed4f441d330c7ee
owlyyy <81492562+arquxx@users.noreply.github.com>
2026-08-23 22:13:05 +0200

    Update download URL for srell package

--- cmake/ports/srell/portfile.cmake
+++ cmake/ports/srell/portfile.cmake
@@ -2,7 +2,7 @@ string(REPLACE "." "_" VERSION ${VERSION})
 
 vcpkg_download_distfile(
     ARCHIVE
-    URLS "https://www.akenotsuki.com/misc/srell/releases/srell${VERSION}.zip"
+    URLS "https://akenotsuki.pages.dev/misc/srell/releases/srell${VERSION}.zip"
     FILENAME "srell${VERSION}.zip"
     SHA512 7c834cdc3b1cc63b3961601429e3d788f182469411aac5377801407550de0313d8dd1422355f1a0d78c77c2b141f75068312140484f4d8f87c3b2e76b4b1c754
 )
