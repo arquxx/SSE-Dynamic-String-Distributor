587d4dbd824597e2d08baf7b3a20de31362e9bb7
owlyyy <81492562+arquxx@users.noreply.github.com>
2026-08-23 22:16:59 +0200

    Change SHA512 checksum for srell download
    Updated the SHA512 checksum for the srell archive download.

--- cmake/ports/srell/portfile.cmake
+++ cmake/ports/srell/portfile.cmake
@@ -4,7 +4,7 @@ vcpkg_download_distfile(
     ARCHIVE
     URLS "https://akenotsuki.pages.dev/misc/srell/releases/srell${VERSION}.zip"
     FILENAME "srell${VERSION}.zip"
-    SHA512 7c834cdc3b1cc63b3961601429e3d788f182469411aac5377801407550de0313d8dd1422355f1a0d78c77c2b141f75068312140484f4d8f87c3b2e76b4b1c754
+    SHA512 0
 )
 
 vcpkg_extract_source_archive(
